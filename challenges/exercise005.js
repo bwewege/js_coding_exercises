f3dfd4e2763d8fc37928bdc7d5cd59fd05fbe476
Brian <brian@vvgonline.com>
2023-05-20 23:46:40 +0100

    Completed all exercise005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,36 +1,101 @@
 export const findNextNumber = (nums, n) => {
-	if (nums === undefined) throw new Error('nums is required');
-	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+  if (nums === undefined) throw new Error("nums is required");
+  if (n === undefined) throw new Error("n is required");
+
+  if (nums.includes(n) === false || nums.indexOf(n) === nums.length - 1) {
+    return null;
+  } else {
+    return nums[nums.indexOf(n) + 1];
+  }
+  // NTS - this can be more concise by using a ternery operator
 };
 
 export const count1sand0s = (str) => {
-	if (str === undefined) throw new Error('str is required');
-	// Your code here!
+  if (str === undefined) throw new Error("str is required");
+
+  let onesCount = 0;
+  let zerosCount = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "1") {
+      onesCount++;
+    } else if (str[i] === "0") {
+      zerosCount++;
+    }
+  }
+
+  return { 1: onesCount, 0: zerosCount };
 };
 
 export const reverseNumber = (n) => {
-	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+  if (n === undefined) throw new Error("n is required");
+
+  const reverseNum = parseInt(n.toString().split("").reverse().join(""));
+
+  return reverseNum;
 };
 
 export const sumArrays = (arrs) => {
-	if (arrs === undefined) throw new Error('arrs is required');
-	// Your code here!
+  if (arrs === undefined) throw new Error("arrs is required");
+
+  let sum = 0;
+
+  for (let i = 0; i < arrs.length; i++) {
+    sum = sum + arrs[i].reduce((a, b) => a + b);
+  }
+
+  return sum;
 };
 
 export const arrShift = (arr) => {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+
+  if (arr.length < 2) {
+    return arr;
+  } else {
+    [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+  }
+
+  return arr;
 };
 
 export const findNeedle = (haystack, searchTerm) => {
-	if (haystack === undefined) throw new Error('haystack is required');
-	if (searchTerm === undefined) throw new Error('searchTerm is required');
-	// Your code here!
+  if (haystack === undefined) throw new Error("haystack is required");
+  if (searchTerm === undefined) throw new Error("searchTerm is required");
+
+  for (let i in haystack) {
+    if (
+      typeof haystack[i] === "string" && //NTS - typeof is used to check if property is a string as includes() works with strings
+      haystack[i].toLowerCase().includes(searchTerm.toLowerCase())
+    ) {
+      return true;
+    }
+  }
+  return false;
 };
 
 export const getWordFrequencies = (str) => {
-	if (str === undefined) throw new Error('str is required');
-	// Your code here!
+  if (str === undefined) throw new Error("str is required");
+
+  const words = str
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9 ]/g, "")
+    .split(" ");
+
+  const wordCount = {};
+
+  for (let i of words) {
+    //NTS - 'of' is used instead of in to iterate over arrays
+    if (i === "") {
+      continue;
+    }
+
+    if (wordCount[i]) {
+      wordCount[i]++;
+    } else {
+      wordCount[i] = 1;
+    }
+  }
+
+  return wordCount;
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-const sentences = [
-  "You should specify a license for your package so that people know how they are permitted to use it",
-  "The main field is a module ID that is the primary entry point to your program",
-  "The repository field should specify the place where your code lives",
-  "The 'scripts' property is a dictionary containing script commands that are run at various times in the lifecycle of your package",
-  "Dependencies are specified in a simple object that maps a package name to a version range",
-  "It's best to map these additional items such as a test framework, which is not needed for running your project, in a devDependencies object",
-  "If you plan to publish your package, the most important things in your package.json are the name and version fields as they will be required",
-  "If you don’t plan to publish your package, the name and version fields are optional",
-  "Put keywords in it. It's an array of strings. This helps people discover your package as it's listed in npm search",
-  "The bugs field should hold the url to your project’s issue tracker and / or the email address to which issues should be reported.",
-];
+const str =
+  "the cat is hairier than the rat, don't think thats all! He is big too";
 
-const str = "Repository";
+const words = str
+  .toLowerCase()
+  .replace(/[^a-zA-Z0-9 ]/g, "")
+  .split(" ");
+console.log(words);
+const wordCount = {};
 
-const sentencesWithStr = sentences.filter((a) =>
-  a.toLowerCase().includes(str.toLowerCase())
-);
+for (let i of words) {
+  //NTS - 'of' is used instead of in to iterate over arrays
+  if (i === "") {
+    continue;
+  }
 
-console.log(sentencesWithStr);
+  if (wordCount[i]) {
+    wordCount[i]++;
+  } else {
+    wordCount[i] = 1;
+  }
+}
+
+console.log(wordCount);
+
+//NTS - may want to figure out how to deal with apostraphe's e.g. don't
